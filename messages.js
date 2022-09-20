db979ba2e971a243db4e87f0652b15326affd797
Nathaniel Lowe <z5361669@ad.unsw.edu.au>
2022-09-20 11:30:53 +1000

    Fixed the stub return value

--- messages.js
+++ messages.js
@@ -1,13 +1,16 @@
-function channelMessagesV1 ( authUserId, channelId, start ) {
-  messages: [
-    {
-      messageId: 1,
-      uId: 1,
-      message: 'Hello world',
-      timeSent: 1582426789,
-    }
-  ],
-  start: 0,
-  end: 50,
+function channelMessagesV1 (authUserId, channelId, start) {
+  
+  return {
+    messages: [
+      {
+        messageId: 1,
+        uId: 1,
+        message: 'Hello world',
+        timeSent: 1582426789,
+      }
+    ],
+    start: 0,
+    end: 50,
+  }
 }
 
