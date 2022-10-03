4d37486e0c324ad325d8dc52d1c273ddc79b0b08
Jadon <z54170255@ad.unsw.edu.au>
2022-10-04 10:19:23 +1100

    implemented channelslist function

--- src/channels.js
+++ src/channels.js
@@ -1,3 +1,5 @@
+import { getData, setData} from './dataStore.js';
+
 function channelsCreateV1( authUserId, name, isPublic ) {
   return { 
     channelId: 1, 
@@ -5,14 +7,68 @@ function channelsCreateV1( authUserId, name, isPublic ) {
 }
 
 function channelsListV1(authUserId) {
-  return {
-    channels: [
-      {
+  setData({
+      'users': [
+        {
+          email: 'cow@gmail.com',
+          authUserId: 1,
+          password: 'turtle',
+          nameFirst: 'cat',
+          nameLast: 'fish',
+          userHandle: 'catfish0'
+        },
+        {
+          email: 'cow1@gmail.com',
+          authUserId: 2,
+          password: 'turtle',
+          nameFirst: 'cat',
+          nameLast: 'fish',
+          userHandle: 'catfish1'
+        },
+      ],
+      'channels': [{
         channelId: 1,
-        name: 'My Channel',
+        name: 'channel1',
+        isPublic: true,
+        owners: [1],
+        members: [1,2],
+      },
+      {
+        channelId: 2,
+        name: 'channel2',
+        isPublic: true,
+        owners: [2],
+        members: [1,2],
       }
-    ],
+      ]
+    
+    });
+
+  const data = getData();
+  const allchannelsArray = data.channels;
+  const publicChannelsArray = [];
+
+  for (const element of allchannelsArray) {
+    if (element.isPublic === true) {
+      publicChannelsArray.push(element);
+    }
+  }
+  
+  for (const element of publicChannelsArray) {
+    if ((element.owners).includes(authUserId)) {
+      console.log('works');
+  }
+  console.log(element.owners);
+
+  }
+  console.log('**');
+  console.log(publicChannelsArray);
+  console.log('**');
+ 
+  return {
+    channels: publicChannelsArray,
   };
+
 }
 
 function channelsListAllV1( authUserId ) { 
@@ -25,5 +81,7 @@ function channelsListAllV1( authUserId ) {
     ],
   };
 }
+
+console.log(channelsListV1(1));
 
 export {channelsCreateV1, channelsListV1, channelsListAllV1};
