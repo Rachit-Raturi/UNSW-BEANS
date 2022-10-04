2f241bd2c2f4ecd527fbdd4857837714e6c8cba9
Aimee Pham <z5420392@ad.unsw.edu.au>
2022-10-04 11:53:26 +1100

    Fixed the beforeEach bit

--- src/channel.test.js
+++ src/channel.test.js
@@ -8,32 +8,26 @@ import ClearV1 from './other';
 
 beforeEach(() => {
   ClearV1();
+  const authUser = authRegisterV1('valid.email@gmail.com', 'aGo0dpAssw0rD', 'Mike', 'Hox');
+  const user = authRegisterV1('also.valid@yahoo.com.au', 'a83tt3rpW0rD', 'Barack', 'Obama');
+  const channel = channelsCreateV1(authUser.authUserId, 'Disney', true);
 });
 
 describe('Tests for channelDetailsV1', () => {
   test('Test 1: Invalid channelId', () => {
-    const user = authRegisterV1('valid.email@gmail.com', 'aGo0dpAssw0rD', 'Mike', 'Hox');
-    const channel = channelsCreateV1(user.authUserId, 'Disney', true);
-    expect(channelDetailsV1(user.authUserId, channel.channelId + 1)).toStrictEqual({error: expect.any(String)});
+    expect(channelDetailsV1(authUser.authUserId, channel.channelId + 1)).toStrictEqual({error: expect.any(String)});
   });
 
   test('Test 2: Invalid authUserId', () => {
-    const user = authRegisterV1('valid.email@gmail.com', 'aGo0dpAssw0rD', 'Mike', 'Hox');
-    const channel = channelsCreateV1(user.authUserId, 'Disney', true);
-    expect(channelDetailsV1(user.authUserId + 1, channel.channelId)).toStrictEqual({error: expect.any(String)});
+    expect(channelDetailsV1(authUser.authUserId + 1, channel.channelId)).toStrictEqual({error: expect.any(String)});
   });
 
   test('Test 3: User is not a member of the channel', () => {
-    const authUser = authRegisterV1('valid.email@gmail.com', 'aGo0dpAssw0rD', 'Mike', 'Hox');
-    const user = authRegisterV1('also.valid@yahoo.com.au', 'a83tt3rpW0rD', 'Barack', 'Obama');
-    const channel = channelsCreateV1(authUser.authUserId, 'Disney', true);
     expect(channelDetailsV1(user.authUserId, channel.channelId)).toStrictEqual({error: expect.any(String)});
   });
 
   test('Test 4: Successful use of channelDetailsV1', () => {
-    const user = authRegisterV1('valid.email@gmail.com', 'aGo0dpAssw0rD', 'Mike', 'Hox');
-    const channel = channelsCreateV1(user.authUserId, 'Disney', true);
-    expect(channelDetailsV1(user.authUserId, channel.channelId)).toStrictEqual({
+    expect(channelDetailsV1(authUser.authUserId, channel.channelId)).toStrictEqual({
       name: 'Disney',
       isPublic: true,
       ownerMembers: [
