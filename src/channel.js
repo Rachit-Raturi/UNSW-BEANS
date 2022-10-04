01aada5c1da677528e158f7c062535adb26c7343
Jadon <z54170255@ad.unsw.edu.au>
2022-10-04 21:29:58 +1100

    fixed error checking

--- src/channel.js
+++ src/channel.js
@@ -6,47 +6,6 @@ function channelJoinV1( authUserId, channelId ) {
 }
 
 function channelInviteV1( authUserId, channelId, uId ) { 
-  
-  setData({
-    'users': [
-      {
-        email: 'cow@gmail.com',
-        authUserId: 1,
-        password: 'turtle',
-        nameFirst: 'cat',
-        nameLast: 'fish',
-        userHandle: 'catfish0'
-      },
-      {
-        email: 'cow1@gmail.com',
-        authUserId: 2,
-        password: 'turtle',
-        nameFirst: 'cat',
-        nameLast: 'fish',
-        userHandle: 'catfish1'
-      },
-    ],
-    'channels': [{
-      channelId: 1,
-      name: 'channel1',
-      isPublic: true,
-      owners: [1],
-      members: [2],
-    },
-    {
-      channelId: 2,
-      name: 'channel2',
-      isPublic: true,
-      owners: [2],
-      members: [],
-    }
-    ]
-  
-  });
-
-
-
-
   const data = getData();
 
   const isvalidAuthuser = data.users.find(a => a.authUserId === authUserId);  
@@ -70,20 +29,17 @@ function channelInviteV1( authUserId, channelId, uId ) {
     };
   }
 
-  const channel_array = data.channels[channelId - 1]
-  console.log(channel_array);
-  console.log(channel_array.members);
-  console.log(channel_array.owners);
-  channel_array.members.push(1);
-  data.channels[channelId - 1] = channel_array;
+  for (element of data.channels[channelId].allMembers) {
+    if (element === authUserId) {
+      return {
+        error: "already a member",
+    };
+    }
+  }
 
 
   return {};
 }
-
-console.log(channelInviteV1(2,2,1))
-console.log(getData().channels[1].members);
-
 /**
  * Given a channel with ID channelId that the authorised user
  * is a member of, provides basic details about the channel.
