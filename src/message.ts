e3ca2a781054509304d0b810a4f215c9fc6b548a
Sam Evstigneev <samevstigneev@Sams-MacBook-Air.local>
2022-10-19 09:17:44 +1100

    messageSend function implementation

---
+++ src/message.ts
@@ -0,0 +1,50 @@
+import { getData, setData } from './dataStore.js';
+
+/**
+ * Given a channel with ID channelId that the authorised user
+ * is a member of, provides basic details about the channel.
+ *
+ * @param {string} token
+ * @param {number} channelId
+ * @param {string} message
+ * @returns {number} messageId
+ */
+
+function messageSend(token: string, channelId: number, message: string) {
+    let data = getData() 
+    if (data.channels[channelId] === undefined) { 
+        return {error: `channelId(${channelId}) does not refer to a valid channel`};
+    }
+
+    if (message.length < 1 || message.length > 1000) { 
+        return {error: `message length(${message.length}) is too long or too short`};
+    }
+
+    const user = data.users.find(a => a.token === token);
+    const checkIsMember = data.channels[channelId].allMembers; 
+
+    if (!checkIsMember.includes(user.uId)) {
+        return {error: `user(${token}) is not a member of channel(${channelId})`};
+    }
+
+    if (user.token === undefined) { 
+        return {error: `token(${token}) does not refer to a valid user`};
+    }    
+
+    let messageId = (new Date()).getTime() + Math.round(Math.random())
+    let time = Math.floor(Date.now() / 1000); 
+
+    let newMessage = { 
+        messageId: messageId, 
+        uId: user.uId,
+        message: message,
+        timeSent: time
+        
+    }
+    data.channels[channelId].messages.push(newMessage)
+    setData(data);
+
+    return messageId;
+}
+
+export { messageSend }
