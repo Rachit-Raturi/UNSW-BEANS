daf0c740ecbc431d4bd67624b4eeae9fcaa73ad0
Jadon <z54170255@ad.unsw.edu.au>
2022-10-23 21:38:34 +1100

    added extractUser function

--- src/helperfunctions.ts
+++ src/helperfunctions.ts
@@ -104,47 +104,5 @@ function findUser(token: string): object {
   return userObject;
 }
 
-
-setData(
-  {users: [
-    {
-      uId: 0,
-      email: 'bot@hotmail.com',
-      password: '12345',
-      nameFirst: 'Beep',
-      nameLast: 'Boop',
-      handleStr: 'beepboop',
-    },
-    {
-      uId: 1,
-      email: 'robot@gmail.com',
-      password: 'qwerty',
-      nameFirst: 'Beep',
-      nameLast: 'Boop',
-      handleStr: 'beepboop0',
-    },
-    {
-      uId: 2,
-      email: 'random@outlook.com',
-      password: 'a8#r2fah51sD',
-      nameFirst: 'this-is@-very',
-      nameLast: 'ab%l0ne',
-      handleStr: 'thisisveryablne',
-    },
-    {
-      uId: 3,
-      email: 'sumeru@gmail.com',
-      password: 'hEter0chTom@tic',
-      nameFirst: 'Candace',
-      nameLast: 'Deshret',
-      handleStr: 'candacedeshret',
-    }
-  ]
-}
-)
-console.log(validHandleStr('app#'));
-console.log(extractUser(0));
-console.log(extractUser());
-
 export { validEmail, validToken, validName, validHandleStr, extractUser, findUser };
 
