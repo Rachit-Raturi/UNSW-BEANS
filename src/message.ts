9e0535749e2bde665f50b7dbd4bc651f84f85103
Sam Evstigneev <samevstigneev@Sams-MacBook-Air.local>
2022-10-25 15:17:35 +1100

    changed Id generation

--- src/message.ts
+++ src/message.ts
@@ -1,4 +1,6 @@
+import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript';
 import { getData, setData } from './dataStore';
+import {findUser} from './helperfunctions'
 
 /**
  * Given a channel with ID channelId that the authorised user
@@ -10,8 +12,10 @@ import { getData, setData } from './dataStore';
  * @returns {number} messageId
  */
 
+let Id = 0; 
 function messageSend(token: string, channelId: number, message: string) {
   const data = getData();
+  
   if (data.channels[channelId] === undefined) {
     return { error: `channelId(${channelId}) does not refer to a valid channel` };
   }
@@ -19,32 +23,32 @@ function messageSend(token: string, channelId: number, message: string) {
   if (message.length < 1 || message.length > 1000) {
     return { error: `message length(${message.length}) is too long or too short` };
   }
-
-  const user = data.users.find(a => a.token === token);
-  const checkIsMember = data.channels[channelId].allMembers;
-
-  if (!checkIsMember.includes(user.uId)) {
-    return { error: `user(${token}) is not a member of channel(${channelId})` };
-  }
-
-  if (user.token === undefined) {
+  
+  let user = findUser(token); 
+  if (user === undefined) {
     return { error: `token(${token}) does not refer to a valid user` };
   }
 
-  const messageId = (new Date()).getTime() + Math.round(Math.random());
+  const checkIsMember = data.channels[channelId].allMembers;
+  if (checkIsMember.includes(user.uId) === false) {
+    return { error: `user(${token}) is not a member of channel(${channelId})` };
+  }
+
   const time = Math.floor(Date.now() / 1000);
 
   const newMessage = {
-    messageId: messageId,
-    uId: user.uId,
+    messageId: Id,
+    uId: 3,
     message: message,
     timeSent: time
 
   };
+
   data.channels[channelId].messages.push(newMessage);
   setData(data);
 
-  return messageId;
+  Id = Id + 2; 
+  return {messageId: Id - 2};
 }
 
 export { messageSend };
