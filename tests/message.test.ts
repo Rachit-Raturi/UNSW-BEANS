a1bec066a4d0c690d6a799bd7c0e8ba378ad1a7a
Sam Evstigneev <samevstigneev@Sams-MacBook-Air.local>
2022-10-25 15:16:52 +1100

    Edited http config for tests

--- tests/message.test.ts
+++ tests/message.test.ts
@@ -1,76 +1,71 @@
-import request from 'sync-request';
 
+import request, { HttpVerb } from 'sync-request';
 import { port, url } from '../src/config.json';
-import { clearV1 } from '../src/other';
-import { channelsCreateV1 } from '../src/channels';
+import { getData } from '../src/dataStore';
+import { messageSend } from '../src/message';
 import { authRegisterV1 } from '../src/auth';
-
+import { channelsCreateV1 } from '../src/channels';
+import { requestChannelsCreate, requestauthRegister, requestMessageSend, requestClear } from './helper'
 const SERVER_URL = `${url}:${port}`;
 
-let user;
-let user1;
-let channel1;
+let user; 
+let user2; 
+let channel;
+let channel2;
 
 beforeEach(() => {
-  clearV1();
-  user = authRegisterV1('test@gmail.com', 'password',
-    'firstname', 'lastname');
-  user1 = authRegisterV1('test1@gmail.com', 'password1',
-    'firstname1', 'lastname1');
-  channel1 = channelsCreateV1(user.authUserId, 'My Channel1', true);
+  requestClear()
+  user = requestauthRegister("email@gmail.com", "password", "firstname", "lastname"); 
+  user2 = requestauthRegister("2email@gmail.com", "2password", "2firstname", "2lastname"); 
+  channel = requestChannelsCreate(user.authUserId, "name", true);
+  channel2 = requestChannelsCreate(user.authUserId, "name", true);
 });
 
-function requestMessageSendV1(token: string, channelId: number, message: string) {
-  const res = request(
-    'POST',
-    SERVER_URL + '/message/send/v1',
-    {
-      json: {
-        token,
-        channelId,
-        message,
-      }
+// ========================================================================= //
+// Message Send Tests 
+
+describe('Tests for Message Send', () => {
+  test('Successful case', () => {
+
+    expect(requestMessageSend(user.token, channel.channelId, "Message")).toStrictEqual({messageId: expect.any(Number)});
+  });
+
+  test('Successful case 1 - Ids are unique', () => { 
+
+    let arr: number[];
+    arr = [];
+    for (let i = 0; i < 10; i++) {
+      arr.push(requestMessageSend(user.token, channel.channelId, 'Message'));
     }
-  );
-  return JSON.parse(res.getBody() as string);
-}
 
-// test('Successful case', () => {
-//   expect(requestMessageSendV1(user.token, channel1.channelId, "Message")).toStrictEqual({messageId: expect.any(Number)});
-// });
+    const unique = Array.from(new Set(arr));
+    expect(unique.length).toStrictEqual(10);
+  });
+  
+  test('Invalid ChannelId', () => { 
+    const invalidChannelId = channel2.channelId + 1;
 
-test('Successful case - Ids are unique', () => {
-  let arr: number[];
-  arr = [];
-  for (let i = 0; i < 10; i++) {
-    arr.push(requestMessageSendV1(user.token, channel1.channelId, 'Message'));
-  }
+    
+    expect(requestMessageSend(user.token, invalidChannelId, 'Message')).toStrictEqual({ error: expect.any(String) });
+  });
 
-  const unique = Array.from(new Set(arr));
-  expect(unique.length).toStrictEqual(10);
-});
+  test('Invalid Token', () => {
+    let invalidToken = "0";
+    expect(requestMessageSend(invalidToken, channel.channelId, "Message")).toStrictEqual({error: expect.any(String)});
+  });
 
-test('Invalid ChannelId', () => {
-  const invalidChannelId = channel1.channelId + 1;
-  expect(requestMessageSendV1(user.token, invalidChannelId, 'Message')).toStrictEqual({ error: expect.any(String) });
-});
+  test('User is not a Member', () => {
+    expect(requestMessageSend(user2.token, channel.channelId, 'Message')).toStrictEqual({ error: expect.any(String) });
+  });
 
-// test('Invalid Token', () => {
-//   let invalidToken = user.token.splice(0, -1);
-//   expect(requestMessageSendV1(invalidToken, channel1.channelId, "Message")).toStrictEqual({error: expect.any(String)});
-// });
+  test('Invalid message lengths', () => {
+    let longString: string;
+    for (let i = 0; i <= 1000; i++) {
+      longString += 'a';
+    }
 
-test('User is not a Member', () => {
-  expect(requestMessageSendV1(user1.token, channel1.channelId, 'Message')).toStrictEqual({ error: expect.any(String) });
-});
-
-test('Invalid message lengths', () => {
-  let longString: string;
-  for (let i = 0; i <= 1000; i++) {
-    longString += 'a';
-  }
-
-  const emptyString = ' ';
-  expect(requestMessageSendV1(user.token, channel1.channelId, longString)).toStrictEqual({ error: expect.any(String) });
-  expect(requestMessageSendV1(user.token, channel1.channelId, emptyString)).toStrictEqual({ error: expect.any(String) });
+    const emptyString = '';
+    expect(requestMessageSend(user.token, channel.channelId, longString)).toStrictEqual({ error: expect.any(String) });
+    expect(requestMessageSend(user.token, channel.channelId, emptyString)).toStrictEqual({ error: expect.any(String) });
+  });
 });
