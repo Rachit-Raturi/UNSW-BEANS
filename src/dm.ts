854efd3bb15eccd92b2bea7fcdd0b2a0d59cb8f2
Aimee Pham <z5420392@ad.unsw.edu.au>
2022-10-28 08:21:24 +1100

    Finished the remaining dm functions

--- src/dm.ts
+++ src/dm.ts
@@ -6,24 +6,13 @@ interface dm {
   name: string
 }
 
-/**
- * Returns the list of DMs that the user is a member of.
- *
- * @param {String} token
- * @returns {Object} dms
- */
-// function dmList (token: string): Array<dm> {
-//   const data = getData();
-
-//   // invalid token error
-//   if (data.users[token] === undefined) {
-//     return {
-//       error: 'Invalid token'
-//     };
-//   }
-
-//   return {};
-// }
+interface user {
+  uId: number,
+  email: string,
+  nameFirst: string,
+  nameLast: string,
+  handleStr: string
+}
 
 function dmCreateV1(token: string, uIds?: Array<number>) {
   const data = getData();
@@ -57,19 +46,14 @@ function dmCreateV1(token: string, uIds?: Array<number>) {
   const currentUser = findUser(token);
   for (const uId of uIds) {
     if (uId === currentUser.uId) {
-      return { error: 'owner is part of uIds'};
+      return { error: 'owner is part of uIds' };
     }
   }
 
-  const ownerHandle: string = currentUser.handleStr;
-  const authUserId: number = currentUser.uId;
-
-  const name = [];
-  name.push(ownerHandle);
+  let name = currentUser.handleStr;
 
   for (const element of uIds) {
-    let userHandle = ', ' + data.users[element].handleStr;
-    name.push(userHandle);
+    name = name + ', ' + data.users[element].handleStr;
   }
   const membersArray = uIds;
   membersArray.push(currentUser.uId);
@@ -77,7 +61,7 @@ function dmCreateV1(token: string, uIds?: Array<number>) {
   const dm = {
     dmId: data.dms.length,
     name: name,
-    owner: authUserId,
+    owner: currentUser.uId,
     members: membersArray,
     messages: [],
   };
@@ -88,6 +72,208 @@ function dmCreateV1(token: string, uIds?: Array<number>) {
   return {
     dmId: data.dms.length - 1,
   };
+}
+
+/**
+ * Returns the list of DMs that the user is a member of.
+ *
+ * @param {String} token
+ * @returns {Array<dm> | Object} dms | error
+ */
+function dmListV1 (token: string): Array<dm> | object {
+  const data = getData();
+
+  // invalid token error
+  if (validToken(token) === false) {
+    return {
+      error: 'Invalid token'
+    };
+  }
+  
+  const currentUser = findUser(token);
+  const dmArray: Array<dm> = [];
+
+  // create array of dms
+  for (const dm of data.dms) {
+    if (dm.members.includes(currentUser.uId)) {
+      dmArray.push({ dmId: dm.dmId, name: dm.name });
+    }
+  }
+
+  return { dms: dmArray };
+}
+
+/**
+ * Remove an existing DM, so all members are no longer in the DM.
+ * This can only be done by the original creator of the DM.
+ *
+ * @param {String} token
+ * @param {Number} dmId
+ * @returns {}
+ */
+ function dmRemoveV1 (token: string, dmId: number) {
+  const data = getData();
+
+  // Invalid token error
+  if (validToken(token) === false) {
+    return {
+      error: 'Invalid token'
+    };
+  }
+
+  // Invalid dmId error
+  const currentDm = data.dms.find(d => d.dmId === dmId);
+  if (currentDm === undefined) {
+    return {
+      error: 'Invalid dmId'
+    };
+  }
+  
+  const currentUser = findUser(token);
+
+  // User is not a member of DM error
+  if (data.dms[dmId].members.includes(currentUser.uId) === false) {
+    return {
+      error: `User(${token}) is not a member of the dm(${dmId})`
+    };
+  
+  }
+
+  // User is not the original DM creator
+  if (currentUser.uId !== data.dms[dmId].owner) {
+    return {
+      error: `User(${token}) is not the original creator of the dm(${dmId})`
+    };
+  }
+  
+
+  const dmArray: Array<dm> = [];
+
+  // Creates a new array of the DMs excluding the one to be removed
+  for (const dm of data.dms) {
+    if (dm.dmId !== dmId) {
+      dmArray.push(dm);
+    }
+  }
+  
+  data.dms = dmArray;
+
+  return {};
+}
+
+/**
+ * Given a DM with ID dmId that the authorised user
+ * is a member of, provide basic details about the DM.
+ *
+ * @param {String} token
+ * @param {Number} dmId
+ * @returns {Object} name, members
+ */
+function dmDetailsV1 (token: string, dmId: number): Object {
+  const data = getData();
+
+  // Invalid token error
+  if (validToken(token) === false) {
+    return {
+      error: 'Invalid token'
+    };
+  }
+
+  // Invalid dmId error
+  const currentDm = data.dms.find(d => d.dmId === dmId);
+  if (currentDm === undefined) {
+    return {
+      error: 'Invalid dmId'
+    };
+  }
+
+  const currentUser = findUser(token);
+
+  // User is not a member of DM error
+  if (data.dms[dmId].members.includes(currentUser.uId) === false) {
+    return {
+      error: `User(${token}) is not a member of the dm(${dmId})`
+    };
+  }
+
+  // Create members array output and set the owner as the first member listed
+  const membersArray: Array<user> = [
+    {
+      uId: currentUser.uId,
+      email: currentUser.email,
+      nameFirst: currentUser.nameFirst,
+      nameLast: currentUser.nameLast,
+      handleStr: currentUser.handleStr
+    }
+  ];
+
+  for (const member of data.dms[dmId].members) {
+    membersArray.push({
+      uId: data.users[member].uId,
+      email: data.users[member].email,
+      nameFirst: data.users[member].nameFirst,
+      nameLast: data.users[member].nameLast,
+      handleStr: data.users[member].handleStr
+    });
+  }
+
+  // Remove the owner because they get repeated at the end
+  membersArray.pop();
+  
+  return {
+    name: currentDm.name,
+    members: membersArray
+  };
+}
+
+/**
+ * Given a DM with ID dmId, the user is removed as a member of this DM.
+ * The creator is allowed to leave and the DM will still exist if this happens.
+ * This does not update the name of the DM.
+ *
+ * @param {String} token
+ * @param {Number} dmId
+ * @returns {}
+ */
+function dmLeaveV1 (token: string, dmId: number) {
+  const data = getData();
+
+  // Invalid token error
+  if (validToken(token) === false) {
+    return {
+      error: 'Invalid token'
+    };
+  }
+
+  // Invalid dmId error
+  const currentDm = data.dms.find(d => d.dmId === dmId);
+  if (currentDm === undefined) {
+    return {
+      error: 'Invalid dmId'
+    };
+  }
+
+  const currentUser = findUser(token);
+
+  // User is not a member of DM error
+  if (data.dms[dmId].members.includes(currentUser.uId) === false) {
+    return {
+      error: `User(${token}) is not a member of the dm(${dmId})`
+    };
+  }
+
+  const membersArray: Array<user> = [];
+
+  // Creates a new array of the members excluding the one to be removed
+  for (const member of data.dms[dmId].members) {
+    if (member.token !== token) {
+      membersArray.push(member);
+    }
+  }
+  
+  data.dms[dmId].members = membersArray;
+
+  return {};
 }
 
 function dmMessagesV1(token: string, dmId: number, start: number) {
@@ -110,16 +296,14 @@ function dmMessagesV1(token: string, dmId: number, start: number) {
   }
 
   const currentUser = findUser(token);
-  const authUserId: number = currentUser.uId;
 
-  // Check if user is a member of dm
-  const checkIsMember = data.dms[dmId].members;
-  if (checkIsMember.includes(authUserId) === false) {
+  // User is not a member of DM error
+  if (data.dms[dmId].members.includes(currentUser.uId) === false) {
     return {
-      error: `user(${token}) is not a member of the dm(${dmId})`
+      error: `User(${token}) is not a member of the dm(${dmId})`
     };
   }
-
+  
   const messages = data.dms[dmId].messages;
   const numberOfMessages = messages.length;
 
@@ -166,7 +350,6 @@ function dmMessagesV1(token: string, dmId: number, start: number) {
     }
   }
 }
-
 
 let messageId = 1;
 function messageSendDmV1(token: string, dmId: number, message: string) {
@@ -224,22 +407,4 @@ function messageSendDmV1(token: string, dmId: number, message: string) {
   };
 }
 
-function dmListV1 (token: string) {
-  return{};
-}
-
-function dmRemoveV1 (token: string, dmId: number) {
-  return{};
-}
-
-function dmDetailsV1 (token: string, dmId: number) {
-  return{};
-}
-
-
-function dmLeaveV1 (token: string, dmId: number) {
-  return{};
-}
-
-
-export { dmCreateV1, dmMessagesV1, messageSendDmV1, dmListV1, dmRemoveV1, dmDetailsV1, dmLeaveV1 };
+export { dmCreateV1, dmListV1, dmRemoveV1, dmDetailsV1, dmLeaveV1, dmMessagesV1, messageSendDmV1 };
