70afbcc5d1654b1c74de305a769c2e0c64808a3f
Jadon <z54170255@ad.unsw.edu.au>
2022-11-10 19:16:06 +1100

    added new server routes for helper.ts

--- tests/helper.ts
+++ tests/helper.ts
@@ -37,103 +37,103 @@ export function requestClear() {
 // =========================================================================
 // Auth functions
 export function requestAuthLogin(email: string, password: string) {
-  return requestHelper('POST', '/auth/login/v2', { email, password });
+  return requestHelper('POST', '/auth/login/v3', { email, password });
 }
 
 export function requestAuthRegister(email: string, password: string, nameFirst: string, nameLast: string) {
-  return requestHelper('POST', '/auth/register/v2', { email, password, nameFirst, nameLast });
+  return requestHelper('POST', '/auth/register/v3', { email, password, nameFirst, nameLast });
 }
 
 export function requestAuthLogout(token: string) {
-  return requestHelper('POST', '/auth/logout/v1', {}, { token: token });
+  return requestHelper('POST', '/auth/logout/v2', {}, { token: token });
 }
 
 // =========================================================================
 // Channels functions
 export function requestChannelsCreate(token: string, name: string, isPublic: boolean) {
-  return requestHelper('POST', '/channels/create/v2', { name, isPublic }, { token: token });
+  return requestHelper('POST', '/channels/create/v3', { name, isPublic }, { token: token });
 }
 
 export function requestChannelsList(token: string) {
-  return requestHelper('GET', '/channels/list/v2', {}, { token: token });
+  return requestHelper('GET', '/channels/list/v3', {}, { token: token });
 }
 
 export function requestChannelsListAll(token: string) {
-  return requestHelper('GET', '/channels/listAll/v2', {}, { token: token });
+  return requestHelper('GET', '/channels/listAll/v3', {}, { token: token });
 }
 
 // =========================================================================
 // Channel functions
 export function requestChannelDetails(token: string, channelId: number) {
-  return requestHelper('GET', '/channel/details/v2', { channelId }, { token: token });
+  return requestHelper('GET', '/channel/details/v3', { channelId }, { token: token });
 }
 
 export function requestChannelJoin(token: string, channelId: number) {
-  return requestHelper('POST', '/channel/join/v2', { channelId }, { token: token });
+  return requestHelper('POST', '/channel/join/v3', { channelId }, { token: token });
 }
 
 export function requestChannelInvite(token: string, channelId: number, uId: number) {
-  return requestHelper('POST', '/channel/invite/v2', { channelId, uId }, { token: token });
+  return requestHelper('POST', '/channel/invite/v3', { channelId, uId }, { token: token });
 }
 
 export function requestChannelMessages(token: string, channelId: number, start: number) {
-  return requestHelper('GET', '/channel/messages/v2', { channelId, start }, { token: token });
+  return requestHelper('GET', '/channel/messages/v3', { channelId, start }, { token: token });
 }
 
 export function requestChannelLeave(token: string, channelId: number) {
-  return requestHelper('POST', '/channel/leave/v1', { channelId }, { token: token });
+  return requestHelper('POST', '/channel/leave/v2', { channelId }, { token: token });
 }
 
 export function requestChannelAddOwner(token: string, channelId: number, uId: number) {
-  return requestHelper('POST', '/channel/addowner/v1', { channelId, uId }, { token: token });
+  return requestHelper('POST', '/channel/addowner/v2', { channelId, uId }, { token: token });
 }
 
 export function requestChannelRemoveOwner(token: string, channelId: number, uId: number) {
-  return requestHelper('POST', '/channel/removeowner/v1', { channelId, uId }, { token: token });
+  return requestHelper('POST', '/channel/removeowner/v2', { channelId, uId }, { token: token });
 }
 
 // =========================================================================
 // DM functions
 export function requestDmCreate(token: string, uIds?: Array<number>) {
-  return requestHelper('POST', '/dm/create/v1', { uIds }, { token: token });
+  return requestHelper('POST', '/dm/create/v2', { uIds }, { token: token });
 }
 
 export function requestDmList(token: string) {
-  return requestHelper('GET', '/dm/list/v1', {}, { token: token });
+  return requestHelper('GET', '/dm/list/v2', {}, { token: token });
 }
 
 export function requestDmRemove(token: string, dmId: number) {
-  return requestHelper('DELETE', '/dm/remove/v1', { dmId }, { token: token });
+  return requestHelper('DELETE', '/dm/remove/v2', { dmId }, { token: token });
 }
 
 export function requestDmDetails(token: string, dmId: number) {
-  return requestHelper('GET', '/dm/details/v1', { dmId }, { token: token });
+  return requestHelper('GET', '/dm/details/v2', { dmId }, { token: token });
 }
 
 export function requestDmLeave(token: string, dmId: number) {
-  return requestHelper('POST', '/dm/leave/v1', { dmId }, { token: token });
+  return requestHelper('POST', '/dm/leave/v2', { dmId }, { token: token });
 }
 
 export function requestDmMessages(token: string, dmId: number, start: number) {
-  return requestHelper('GET', '/dm/messages/v1', { dmId, start }, { token: token });
+  return requestHelper('GET', '/dm/messages/v2', { dmId, start }, { token: token });
 }
 
 // =========================================================================
 // Message Function
 export function requestMessageSend(token: string, channelId: number, message: string) {
-  return requestHelper('POST', '/message/send/v1', { channelId, message }, { token: token });
+  return requestHelper('POST', '/message/send/v2', { channelId, message }, { token: token });
 }
 
 export function requestMessageEdit(token: string, messageId: number, message: string) {
-  return requestHelper('PUT', '/message/edit/v1', { messageId, message }, { token: token });
+  return requestHelper('PUT', '/message/edit/v2', { messageId, message }, { token: token });
 }
 
 export function requestMessageRemove(token: string, messageId: number) {
-  return requestHelper('DELETE', '/message/remove/v1', { messageId }, { token: token });
+  return requestHelper('DELETE', '/message/remove/v2', { messageId }, { token: token });
 }
 
 export function requestMessageSendDm(token: string, dmId: number, message: string) {
-  return requestHelper('POST', '/message/senddm/v1', { dmId, message }, { token: token });
+  return requestHelper('POST', '/message/senddm/v2', { dmId, message }, { token: token });
 }
 
 // =========================================================================
